4fde2e91fc319c357884e0012a92f2f38f62e21c
Michel Théo <theo.michel2@outlook.fr>
2021-12-13 13:42:02 +0100

    base html

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+let getDeviseToConvert = document.getElementById('deviseToConvert');
+let getAmountDevise = document.getElementById('numberToConvert');
+let getFinalDevise = document.getElementById('deviseResult');
