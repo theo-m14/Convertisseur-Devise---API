9ddf5680fecf93aac20a32e89124cf195e563da7
Michel Théo <theo.michel2@outlook.fr>
2021-12-13 16:40:37 +0100

    js : connect api and convert script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 let getDeviseToConvert = document.getElementById('deviseToConvert');
 let getAmountDevise = document.getElementById('numberToConvert');
 let getFinalDevise = document.getElementById('deviseResult');
+let url ;
+let getResult = document.getElementById('result');
+
+document.getElementById('convertBtn').addEventListener('click', function(){
+    url = `https://freecurrencyapi.net/api/v2/latest?apikey=c41d1e40-5c1e-11ec-9f38-299e83188014&base_currency=${getDeviseToConvert.value}`;
+    fetch(url)
+        .then(res => res.json() )
+        .then(data => {
+            getResult.innerText = data.data[getFinalDevise.value]*getAmountDevise.value;
+        })
+        .catch(err => console.log("Code de l'erreur:  " + err))
+})
